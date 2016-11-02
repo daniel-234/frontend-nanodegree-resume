3197c9e611a238646130c3c2b5b563ff8679b450
Daniele <daniele.rb6@gmail.com>
2016-11-02 16:04:05 +0100

    refactor: Change Linkedin icon

--- js/helper.js
+++ js/helper.js
@@ -17,7 +17,7 @@ var HTMLheaderRole = '<h3 class="dark-gray-text role-header">%data%</h3>';
 
 var HTMLcontactGeneric = '<a class="flex-item"><span class="sand-text">%contact%</span><span class="white-text">%data%</span></a>';
 var HTMLemail = '<a class="flex-item" href="mailto:%data%"><span class="white-text fa fa-envelope-o fa-lg" aria-hidden="true"></span></a>';
-var HTMLlinkedin = '<a class="flex-item" href="https://uk.linkedin.com/in/%data%" target="_blank"><span class="white-text fa fa-linkedin-square fa-lg" aria-hidden="true"></span></a>';
+var HTMLlinkedin = '<a class="flex-item" href="https://uk.linkedin.com/in/%data%" target="_blank"><span class="white-text fa fa-linkedin fa-lg" aria-hidden="true"></span></a>';
 var HTMLtwitter = '<a class="flex-item" href="https://twitter.com/%data%" target="_blank"><span class="white-text fa fa-twitter fa-lg" aria-hidden="true"></span></a>';
 var HTMLgithub = '<a class="flex-item" href="https://github.com/%data%" target="_blank"><span class="white-text fa fa-github fa-lg" aria-hidden="true"></span></a>';
 var HTMLblog = '<a class="flex-item"><span class="sand-text">blog</span><span class="white-text">%data%</span></a>';
